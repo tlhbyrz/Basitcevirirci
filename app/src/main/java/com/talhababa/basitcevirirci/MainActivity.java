d81efc17549ab8ae859ee4ceae7f6330578f591d
tlhbyrz <talhaboyraz13@gmail.com>
2019-03-19 23:43:57 +0300

    Initial commit

---
+++ app/src/main/java/com/talhababa/basitcevirirci/MainActivity.java
@@ -0,0 +1,17 @@
+package com.talhababa.basitcevirirci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn = findViewById(R.id.bu)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
